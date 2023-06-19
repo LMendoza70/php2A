f025d3b6ff75b83ce47b1b3fc4733934ba9a9805
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-06-19 09:56:51 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   app/Model/UserModel.php

--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -10,5 +10,46 @@
             //creamos la instancia de la coneccion a la base de datos en dbconnection
             $this->dbconnection=new BDConnection();
         }
+
+        //vamos a crear todos los metodos que requieran coneccion a la base de datos en la instancia 
+        //user
+        public function getAll(){
+            //creamos la consulta a ejecutar
+            $sql='SELECT * FROM user';
+            //obtenemos la coneccion a la base de datos 
+            $connection=$this->dbconnection->getConnection();
+            //ejecutamos la consulta
+            $result=$connection->query($sql);
+            //creamos un arreglo para manipulat a result
+            $users=array();
+            //vamos a decomponer a result desde un ciclo
+            while($user=$result->fethc_assoc()){
+                $users[]=$user;
+            }
+            //cerramos la coneccion a la base de datos 
+            $this->dbconnection->closeConnection();
+            //arrojamos la respuesta de nuestra consulta "users"
+            return $users
+        }
+
+        //metodo para obtener a un usuario por su ID
+        public function getById($id){
+            //creamos consulta
+            $sql="SELECT * FROM user WHERE IdUser=$id";
+            //obtenemos la coneccion 
+            $connection=$this->dbconnection->getConnection();
+            //ejecutamos la consulta
+            $reslt=$connection->query($sql);
+            //verificamos que traiga datos y los sacamos a una variable
+            if($reslt && $reslt->num_rows > 0){
+                $user=$reslt->fethc_assoc();
+            }else{
+                $user=false;
+            }
+            //cerramos la coneccion
+            -$this->dbconnection->closeConnection();
+            //arrojamos resultados
+            return $user;
+        }
     }
 ?>
